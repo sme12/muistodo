e9089a924a28ba4d3c4b84987d2c344d203e40c4
sme12 <sazanovvs@gmail.com>
2024-09-30 00:30:00 +0300

    temporary avoid seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,49 +1,43 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 export async function seed() {
-  const email = process.env.SEED_USER_EMAIL ?? "test@test.com";
-
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
-
-  const hashedPassword = await bcrypt.hash(
-    process.env.SEED_USER_PASSWORD ?? "P@ssw0rd",
-    10,
-  );
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  console.log(`Database has been seeded. 🌱`);
+  // const email = process.env.SEED_USER_EMAIL ?? "test@test.com";
+  // // cleanup the existing database
+  // await prisma.user.delete({ where: { email } }).catch(() => {
+  //   // no worries if it doesn't exist yet
+  // });
+  // const hashedPassword = await bcrypt.hash(
+  //   process.env.SEED_USER_PASSWORD ?? "P@ssw0rd",
+  //   10,
+  // );
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email,
+  //     password: {
+  //       create: {
+  //         hash: hashedPassword,
+  //       },
+  //     },
+  //   },
+  // });
+  // await prisma.note.create({
+  //   data: {
+  //     title: "My first note",
+  //     body: "Hello, world!",
+  //     userId: user.id,
+  //   },
+  // });
+  // await prisma.note.create({
+  //   data: {
+  //     title: "My second note",
+  //     body: "Hello, world!",
+  //     userId: user.id,
+  //   },
+  // });
+  // console.log(`Database has been seeded. 🌱`);
 }
 
 seed()
